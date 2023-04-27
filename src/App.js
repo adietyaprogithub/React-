cf9d234e56871aec89ce3387f844d80b0171e34d
adietyaprogithub <adityashende9922@gmail.com>
2023-04-27 07:56:11 +0530

    counter_app

--- src/App.js
+++ src/App.js
@@ -2,25 +2,27 @@ import React from "react";
 import { useState } from "react";
 
 export default function App() {
-  
-const [name , setName] = useState('')
+  const [count, Setcount] = useState(0);
 
-const handler = (e) => {
-
-  setName (e.target.value)
-}
-
-return(
-
-  <div>
-    <h2> name is {name}</h2>
-    <input type="text"  placeholder="Enter Your name " value={name} onChange={handler}/>
-  </div>
-)
-
-
+  const handleincrese = () => {
+    Setcount(count + 1);
   };
 
-  
-  
+  const handledecrese = () => {
+    if ( count >0 ){
+      Setcount (count - 1)
+    }
+    else
+    {
+      Setcount (0)
+    }
+  };
 
+  return (
+    <div>
+      <h1>count {count}</h1>
+      <button onClick={handleincrese}>Increment</button>
+      <button onClick={handledecrese}>Decrement</button>
+    </div>
+  );
+}
---
+++ src/components/Name_updater/main.js
@@ -0,0 +1,26 @@
+// import React from "react";
+// import { useState } from "react";
+
+// export default function App() {
+  
+// const [name , setName] = useState('')
+
+// const handler = (e) => {
+
+//   setName (e.target.value)
+// }
+
+// return(
+
+//   <div>
+//     <h2> name is {name}</h2>
+//     <input type="text"  placeholder="Enter Your name " value={name} onChange={handler}/>
+//   </div>
+// )
+
+
+//   };
+
+  
+  
+
