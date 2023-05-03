ca9aa35afa441f1463da9c46e333d338c644c65c
adietyaprogithub <adityashende9922@gmail.com>
2023-05-03 08:28:30 +0530

    fetch-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [info, setInfo] = useState([]);
+
+  async function getDetail() {
+    try {
+      const response = await fetch(" https://reqres.in/api/users/");
+      const data = await response.json();
+      setInfo(data.data);
+    } catch (error) {
+      console.log("if the error come ", error);
+    }
+  }
+
+  function Jim() {
+    return(
+      <div>
+      <button onClick={getDetail}> click me </button>
+      <ul>
+        {info.map((user) => (
+          <li key={user.id}>
+            <div> Id : {user.id}</div>
+            <div> Email: {user.email}</div>
+            <div>FIrst_name :{user.first_name}</div>
+            <div>Last_Name: {user.last_name}</div>
+            <div>
+              Avatar : <img src={user.avatar} alt="" />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Jim />
     </div>
   );
 }
-
-export default App;
