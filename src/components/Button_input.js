8266cee17f64b99a3ccb1df73d3f348aac82409c
adietyaprogithub <adityashende9922@gmail.com>
2023-05-04 13:25:31 +0530

    input-button

---
+++ src/components/Button_input.js
@@ -0,0 +1,23 @@
+// import React from "react";
+// import { useState } from "react";
+
+// export default function App() {
+//   const [number, setNumber] = useState();
+//   const [message, setMessage] = useState();
+
+//   function handler(e) {
+//     setNumber(parseInt(e.target.value));
+//   }
+
+//   function newHandler() {
+//     setMessage(number);
+//   }
+
+//   return (
+//     <div>
+//       <input type="text" onChange={handler} />
+//       <button onClick={newHandler}> Hello Click me</button>
+//       <h1>the number is {message}</h1>
+//     </div>
+//   );
+// }
