a153e2169ec9f0023ab446f1fdc1e0b81cdac43a
adietyaprogithub <adityashende9922@gmail.com>
2023-05-08 18:15:38 +0530

    media

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from "react";
-import axios from "axios";
-import { useState } from "react";
-
-
+import "./App.css";
+import { FaSearch } from "react-icons/fa";
 
 export default function App() {
-  const [data, setData] = useState();
-
-  async function dog() {
-    try {
-      const responce = await axios("https://dog.ceo/api/breeds/image/random");
-      setData(responce.data.message);
-    } catch (error) {
-      console.log(error.error);
-    }
-  }
-
   return (
-    <div>
-      <button onClick={dog}> Click Me </button>
-      <img src={data} alt="" />
+    <div className="box">
+      <div className="container">
+        <FaSearch className="icon" />
+        <p> Home</p>
+        <p> About </p>
+        <p> Info</p>
+        <p> contact us</p>
+      </div>
+
+      <div className="second">
+        <img
+          className="imager"
+          src="https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad non
+          perferendis voluptatum porro eum corporis odio eligendi deleniti
+          molestiae possimus repudiandae corrupti dolorem tempora odit
+          blanditiis ut provident, veniam temporibus.
+        </div>
+      </div>
     </div>
   );
 }
