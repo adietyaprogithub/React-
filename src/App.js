2846e63776f501adbb655ba87875baa12f402cae
adietyaprogithub <adityashende9922@gmail.com>
2023-05-17 10:09:44 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 export default function App() {
   const [state, setstate] = useState([1, 2, 3, 4, 5, 6]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddNumber = () => {
+    if (inputValue !== "") {
+      const newNumber = parseInt(inputValue);
+      setstate([...state, newNumber]);
+      setInputValue("");
+    }
+  };
 
   const handleclick = (num) => {
-    const narr = state.filter((n) => n !== num);
-
-    return setstate(narr);
+    const updatedState = state.filter((n) => n !== num);
+    setstate(updatedState);
   };
 
   return (
     <div>
-      {state.map((num) => {
-        return (
-          <p key={num} onClick={() => handleclick(num)}>
-            {num}
-          </p>
-        );
-      })}
+      {state.map((num) => (
+        <p key={num} onClick={() => handleclick(num)}>
+          {num}
+        </p>
+      ))}
+
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Enter a number"
+      />
+      <button onClick={handleAddNumber}>Add Number</button>
     </div>
   );
 }
